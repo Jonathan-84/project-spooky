c18c74b776fb57ee438495e2ec2378f423e8e83f
Jonathan Robinson <72467031+Jonathan-84@users.noreply.github.com>
2022-08-28 11:17:03 -0400

    Updates

---
+++ precache-manifest.b8b04b8db95376e71cf537bae0afa8b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a76d5d5f40857387083",
+    "url": "/project-spooky/static/css/main.134bbdbe.chunk.css"
+  },
+  {
+    "revision": "5a76d5d5f40857387083",
+    "url": "/project-spooky/static/js/main.5a76d5d5.chunk.js"
+  },
+  {
+    "revision": "aa1ef92c5cf7728f9d0a",
+    "url": "/project-spooky/static/js/1.aa1ef92c.chunk.js"
+  },
+  {
+    "revision": "8e0501ff561dc7d0d6f8",
+    "url": "/project-spooky/static/js/2.8e0501ff.chunk.js"
+  },
+  {
+    "revision": "cc6bdb30eb0868b66340",
+    "url": "/project-spooky/static/js/runtime~main.cc6bdb30.js"
+  },
+  {
+    "revision": "21ccafe4754835a8bf524afda27b9a15",
+    "url": "/project-spooky/static/media/sleepy-hollow.21ccafe4.jpg"
+  },
+  {
+    "revision": "a18e141446d3953a9fbe0e8148d37c65",
+    "url": "/project-spooky/index.html"
+  }
+];
